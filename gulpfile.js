aba31728169416f653356bee3fa59f9ac6597c1c
Abdul-Kudus Alhassan <abdulkudusalhassan237@gmail.com>
2025-02-22 22:56:43 -0500

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@
 
 var gulp          = require('gulp');
 var sass          = require('gulp-sass')(require('sass'));
-var browserSync   = require('browser-sync').create();
 var sourcemaps    = require('gulp-sourcemaps');
 var autoprefixer  = require('gulp-autoprefixer');
 var rename        = require('gulp-rename');
 var rtlcss        = require('gulp-rtlcss');
 var gulpif        = require('gulp-if');
+var browserSync   = require('browser-sync').create();
 
 // RTL
-var enableRTL     = false; // TODO: RTL CSS will be only generated if this is TRUE
+var enableRTL     = false;
 
 // Paths
 var Paths = {
@@ -19,14 +19,14 @@ var Paths = {
     CSS: 'template/assets/css/',
     JS: 'template/**/*.js',
     HTML: 'template/**/*.html'
-}
+};
 
-// Compile sass into CSS
+// ✅ Compile Sass into CSS
 gulp.task('sass', function() {
     return gulp.src(Paths.SCSS)
       .pipe(sourcemaps.init())
       .pipe(sass.sync({
-          outputStyle: 'expanded'
+          outputStyle: 'compressed' // Minified for production
       }).on('error', sass.logError)) 
       .pipe(autoprefixer({
           overrideBrowserslist: ['last 2 versions']
@@ -35,12 +35,10 @@ gulp.task('sass', function() {
       .pipe(gulpif(enableRTL, rtlcss()))
       .pipe(gulpif(enableRTL, rename({ suffix: '-rtl' })))
       .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(Paths.CSS))
-      .pipe(browserSync.stream());
+      .pipe(gulp.dest(Paths.CSS));
 });
 
-
-// Static Server
+// ✅ Local Development Server (Only for Running Locally)
 gulp.task('serve', function(done) {
     browserSync.init({
       server: Paths.TEMPLATE
@@ -48,12 +46,16 @@ gulp.task('serve', function(done) {
     done();
 });
 
-// watching files
+// ✅ Watch Files for Changes (Only for Local Development)
 gulp.task('watch', function(done) {
-    gulp.watch(Paths.SCSS).on('change', gulp.series('sass'));
+    gulp.watch(Paths.SCSS, gulp.series('sass'));
     gulp.watch(Paths.HTML).on('change', browserSync.reload);
     gulp.watch(Paths.JS).on('change', browserSync.reload);
     done();
 });
 
+// ✅ Default Task (Local Development) - Does NOT Run on Netlify
 gulp.task('default', gulp.series('sass', 'serve', 'watch'));
+
+// ✅ Netlify-Specific Build Task (Prevents Deployment Issues)
+gulp.task('netlify-build', gulp.series('sass'));
